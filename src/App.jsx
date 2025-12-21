97cc775c9a8c6d904b929b3c906d15a32faf306b
ahmedsami019 <easami019192@gmail.com>
2025-12-21 22:31:07 +0600

    configer ui to send data into database

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Users from './Components/Users'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>simple  CRUD</h1>
+      <Users></Users>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Users.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Users = () => {
+
+    const handleAddUser = (e)=>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const newUser = {name, email}
+        console.log(newUser);
+
+        fetch("http://localhost:3002/users", {
+            method: "POST", 
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newUser)
+        }).then(res => res.json())
+        .then(data => console.log(data))
+    }
+
+    return (
+        <div>
+            <div>
+                <form onSubmit={handleAddUser}>
+                    <input type="text" name='name' /><br />
+                    <input type="email" name='email' /> <br />
+                    <input type="submit" value="add user" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Users;
