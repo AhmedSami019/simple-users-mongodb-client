b272e4e9a8de38dc7234836204e77ef84aad6bcb
ahmedsami019 <easami019192@gmail.com>
2025-12-22 11:00:54 +0600

    update user after delete

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,44 +1,77 @@
-import React, { use } from 'react';
+import React, { use, useState } from "react";
 
-const Users = ({usersPromise}) => {
+const Users = ({ usersPromise }) => {
+  const initialUsers = use(usersPromise);
+  console.log(initialUsers);
 
-    const initialUsers = use(usersPromise)
-    console.log(initialUsers);
+  // states
+  const [users, setUsers] = useState(initialUsers);
 
-    const handleAddUser = (e)=>{
-        e.preventDefault()
-        const name = e.target.name.value
-        const email = e.target.email.value
-        const newUser = {name, email}
-        console.log(newUser);
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newUser = { name, email };
+    console.log(newUser);
 
-        fetch("http://localhost:3002/users", {
-            method: "POST", 
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(newUser)
-        }).then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.insertedId){
-                alert("user added successfully")
-                e.target.reset()
-            }
-        })
-    }
+    fetch("http://localhost:3002/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          newUser._id = data.insertedId;
+          const newUsers = [...users, newUser];
+          setUsers(newUsers);
+          alert("user added successfully");
+          e.target.reset();
+        }
+      });
+  };
 
-    return (
-        <div>
-            <div>
-                <form onSubmit={handleAddUser}>
-                    <input type="text" name='name' /><br />
-                    <input type="email" name='email' /> <br />
-                    <input type="submit" value="add user" />
-                </form>
-            </div>
-        </div>
-    );
+  //   to delete user
+  const handleDeleteUser = (id) => {
+    console.log("delete this user", id);
+    fetch(`http://localhost:3002/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount) {
+          console.log("after delete ", data);
+          const remainingUsers = users.filter(user => user._id !== id)
+          setUsers(remainingUsers)
+        }
+      });
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleAddUser}>
+          <input type="text" name="name" />
+          <br />
+          <input type="email" name="email" /> <br />
+          <input type="submit" value="add user" />
+        </form>
+      </div>
+
+      {/* view users */}
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} / {user.email}
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
